5d9c5e357422f09c16c09e9389f129848bfb3866
sushanth-hi <sushanth.h@slice.com>
2018-05-19 01:03:34 +0530

    Changes in Seed file

--- seeders/20180516141722-demo-user.js
+++ seeders/20180516141722-demo-user.js
@@ -3,18 +3,18 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
      let data = [
-      { id: 1, username: 'Wendy' },
-      { id: 2, username: 'Shannon' },
-      { id: 3, username: 'Tim Annan' },
-      { id: 4, username: 'PG Herveou' },
-      { id: 5, username: 'Mai Richardson' },
-      { id: 6, username: 'Steve Warshaw' },
-      { id: 7, username: 'Jojo Hedaya' },
-      { id: 8, username: 'Chris Tian' },
-      { id: 9, username: 'Sagar Gokhale' },
-      { id: 10, username: 'Thomas Louis' },
-      { id: 11, username: 'Fabien Pasquet' },
-      { id: 12, username: 'Vincent Desmares' }
+      { id: 1, username: 'Parthiv Patel' },
+      { id: 2, username: 'Virat Kohli' },
+      { id: 3, username: 'ABD Villers' },
+      { id: 4, username: 'Mandeep Singh' },
+      { id: 5, username: 'Colin degrandhomme' },
+      { id: 6, username: 'Sarfarz Khan' },
+      { id: 7, username: 'Moeen Ali' },
+      { id: 8, username: 'Tim Southee' },
+      { id: 9, username: 'Yuzivendar Chahal' },
+      { id: 10, username: 'Umesh Yadav' },
+      { id: 11, username: 'Mohammed Siraj' },
+      { id: 12, username: 'Murugan Ashwin' }
     ].map(({id, username}) => ({
       id: id,
       username: username,
